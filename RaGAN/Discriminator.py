19e24fd404cb54eee33dfe66abf4a110d911f942
Anatoliy Mastryukov <admastryukov@avito.ru>
2020-11-15 18:23:55 +0300

    dis fight 6

--- RaGAN/Discriminator.py
+++ RaGAN/Discriminator.py
@@ -6,20 +6,25 @@ from .Dense2DBlock import Dense2DBlock
 
 class Discriminator(nn.Module):
     def __init__(self, params):
-        super( Discriminator, self).__init__()
+        super(Discriminator, self).__init__()
         self.global_branch = nn.Sequential(*[Conv2dBlock(pars) for pars in params["global_branch"]])
-        self.dense_global_branch = nn.Sequential(*[Dense2DBlock(params["dense_global_branch"])  ])
+        self.dense_global_branch = nn.Sequential(*[Dense2DBlock(params["dense_global_branch"])])
 
         self.local_branch = nn.Sequential(*[Conv2dBlock(pars) for pars in params["local_branch"]])
         self.dense_local_branch = nn.Sequential(*[Dense2DBlock(params["dense_local_branch"])])
 
         self.neck = nn.Sequential(*[Dense2DBlock(params["neck"])])
+        self.last_act = nn.Sigmoid()
 
-    def forward(self, x, **kwargs):
+    def forward(self, x, sig=True, **kwargs):
         x1 = self.global_branch(x)
+        # print(x1.shape)
+        x1 = self.dense_global_branch(x1.view(x1.shape[0], -1))
 
-        x1 = self.dense_global_branch(x1.view(-1,2048))
         x2 = self.local_branch(x)
-
-        x2 = self.dense_local_branch(x2.view(-1,2048))
-        return self.neck(torch.cat([x1,x2],1))
+        x2 = self.dense_local_branch(x2.view(x2.shape[0], -1))
+        print(torch.cat([x1, x2], 1).shape)
+        y = self.neck(torch.cat([x1, x2], 1))
+        if sig:
+            y = self.last_act(y)
+        return y
