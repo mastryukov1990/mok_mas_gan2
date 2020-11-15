23567647d0abb354a0191c30b700a6fa6c3dc569
Anatoliy Mastryukov <admastryukov@avito.ru>
2020-11-15 18:11:34 +0300

    fml corrected

--- RaGAN/FML.py
+++ RaGAN/FML.py
@@ -6,17 +6,16 @@ import torch
 
 
 class FML(Loss):
-    def __init__(self, net, n, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(FML, self, ).__init__()
-        self.net = give_blocks(net, n)
 
     @staticmethod
     def norm1(x, y):
         return torch.mean(abs(x - y))
 
-    def forward(self, x, y, *args, **kwargs):
+    def forward(self, x, y, net, *args, **kwargs):
         error = torch.zeros([1])
-        for block in self.net:
+        for block in net:
             x, y = block(x), block(y)
             error += FML.norm1(x, y)
         return error
