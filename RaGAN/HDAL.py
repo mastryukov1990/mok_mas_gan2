bf2571da0870c59dbfa8f5ed922a1e83b071d79a
Anatoliy Mastryukov <admastryukov@avito.ru>
2020-12-21 23:10:07 +0300

    HDAL and HGAL fixed

--- RaGAN/HDAL.py
+++ RaGAN/HDAL.py
@@ -5,7 +5,7 @@ from .Loss import Loss
 
 class HDAL(Loss):
     def __init__(self):
-        super(HGAL, self).__init__()
+        super(HDAL, self).__init__()
 
     def forward(self, y_pred_real, y_pred_fake):
         return mn(ReLU()(1.0 - (y_pred_real - mn(y_pred_fake)))+ReLU()(1.0 + (y_pred_fake - mn(y_pred_real))))/2
